db39af4e91fa6eeef0f18ffdbc4394d992fb98e5
Samuel Mardirosian <smardirosian@pedaids.org>
2018-09-26 16:56:10 -0400

    update moby dick text, add paragraph parsing

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from mastodon import Mastodon
+import re
 
 #   Set up Mastodon
 mastodon = Mastodon(
@@ -6,4 +7,50 @@ mastodon = Mastodon(
     api_base_url = 'https://botsin.space/'
 )
 
-mastodon.status_post("test post!")
+#mastodon.status_post("test post!", spoiler_text="Moby Dick")
+
+format_chars = r'[_]'
+
+
+text_file = open("dick.txt", "r", encoding="utf8")
+
+
+lines = text_file.readlines()
+
+paragraph = ""
+para_line = 0
+prev_paragraph = ""
+prev_para_line = 0
+
+count = 0
+current_chapter = ""
+
+for i, line in enumerate(lines):
+
+	# GROUPING
+
+	if re.match(r'^CHAPTER \d+\.', line):
+		current_chapter = line[:-1]
+	else:
+		if line != "\n":
+			paragraph += re.sub(format_chars, '', line[:-1]) + " "
+
+		else:
+			if len(paragraph) + len(prev_paragraph) < 400:
+
+				prev_paragraph += "\n" + paragraph
+				paragraph = ""
+				para_line = i
+
+			else if len(paragraph) < 400:
+
+				prev_paragraph, prev_para_line = paragraph, para_line
+				paragraph = ""
+				para_line = i
+
+			else if len(prev_paragraph) < 10
+
+
+
+
+print(count)
